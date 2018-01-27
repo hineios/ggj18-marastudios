631b04f00029c375f8144e2f4c0ba886397dfbc4
Baltazar95 <tiago_mpb95@hotmail.com>
2018-01-27 16:53:44 +0000

    Added smoothie controller

---
+++ Assets/Scripts/SmoothieController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothieController : MonoBehaviour {
+
+    public float smoothieStrenght;
+
+    private Rigidbody rig;
+    // Use this for initialization
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.AddForce(Vector3.right * -smoothieStrenght);
+    }
+}
