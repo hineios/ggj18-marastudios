8c416fbe530d672c5b3146e45ed0325eab0b2d35
Fábio Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2018-01-27 06:05:19 +0000

    Added Jump and Walking animations to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,53 +4,80 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Jump Stuff
     public float FallMultiplier = 2.5f;
     public float LowJumpMultiplier = 2f;
     public float JumpVelocity = 7f;
     public int MaxJumps = 1;
     public int JumpCount = 0;
+    private bool Grounded;
 
+    // Move Stuff
     public float HorizontalSpeed = 2.0f;
 
-    private Rigidbody rig;
+    private Rigidbody rb;
+    private SpriteRenderer sr;
+    private Animator anim;
 
-    private void Start()
+    private void Awake()
     {
-        rig = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         JumpCount = MaxJumps;
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        anim.SetFloat("VelocityY", rb.velocity.y);
+        anim.SetFloat("VelocityX", rb.velocity.x);
+        anim.SetBool("Grounded", Grounded);
+
         float hAxis = Input.GetAxis("Horizontal");
-        float vAxis = Input.GetAxis("Vertical");
+        //float vAxis = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(hAxis, 0.0f, 0.0f) * HorizontalSpeed * Time.deltaTime;
-        rig.transform.Translate(movement);
+        rb.transform.Translate(movement);
+
+        //anim.SetFloat("VelocityX", movement.x);
+        
+
+        bool flip = sr.flipX ? movement.x < 0.01f : movement.x > 0.01f;
+        if (flip)
+        {
+            sr.flipX = !sr.flipX;
+        }
 
         if (Input.GetButtonDown("Jump"))
         {
             if (JumpCount > 0)
             {
-                rig.velocity = Vector3.up * JumpVelocity;
+                Grounded = false;
+                
+                rb.velocity = Vector3.up * JumpVelocity;
                 JumpCount -= 1;
             }
         }
 
-        
         // Make jump better, slow down up, fasten down
-        if(rig.velocity.y < 0)
+        if(rb.velocity.y < 0)
         {
-            rig.velocity += Vector3.up * Physics.gravity.y * (FallMultiplier - 1) * Time.deltaTime;
-        } else if(rig.velocity.y > 0 && !Input.GetButton("Jump"))
+            rb.velocity += Vector3.up * Physics.gravity.y * (FallMultiplier - 1) * Time.deltaTime;
+        } else if(rb.velocity.y > 0 && !Input.GetButton("Jump"))
         {
-            rig.velocity += Vector3.up * Physics.gravity.y * (LowJumpMultiplier - 1) * Time.deltaTime;
+            rb.velocity += Vector3.up * Physics.gravity.y * (LowJumpMultiplier - 1) * Time.deltaTime;
         }
+
+        // Update animator stuff
+        
+        
     }
     void OnCollisionEnter(Collision Col)
     {
         if (Col.gameObject.layer == LayerMask.NameToLayer("Default"))
         {
+            Grounded = true;
+            //anim.SetBool("Grounded", Grounded);
             JumpCount = MaxJumps;
         }
     }
