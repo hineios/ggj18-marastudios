c2bec915ecb87a7be911080958e897182a798905
Miguel Oliveira <miguel.oliveira.f@hotmail.com>
2018-01-27 03:55:12 +0000

    melhor scene

---
+++ Assets/Scripts/PlayerController_1.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_1 : MonoBehaviour
+{
+    public float hSpeed = 2.0f;
+    public float vSpeed = 15.0f;
+
+    float distToGround;
+
+    public LayerMask groundLayers;
+    private Rigidbody rig;
+    private BoxCollider col;
+
+    private void Start()
+    {
+        distToGround = GetComponent<Collider>().bounds.extents.y;
+      
+
+        rig = GetComponent<Rigidbody>();
+        col = GetComponent<BoxCollider>();
+    }
+
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0.0f, 0.0f) * hSpeed * Time.deltaTime;
+
+
+       // Debug.Log(Input.GetJoystickNames()[0]);
+
+       /* if (IsGrounded() && ((Input.GetJoystickNames()[0].Contains("Xbox") && Input.GetKeyDown("joystick button 0")) || (Input.GetJoystickNames()[0].Contains("360") && Input.GetKeyDown("joystick button 1"))))
+        {
+            rig.AddForce(Vector3.up * vSpeed, ForceMode.Impulse);
+            //rig.velocity = Vector3.up * vSpeed;
+        } */
+
+        if(IsGrounded() && Input.GetKeyDown("space")) rig.AddForce(Vector3.up * vSpeed, ForceMode.Impulse);
+
+        rig.transform.Translate(movement);
+
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+}
--- Assets/Scripts/TransitionController.cs
+++ Assets/Scripts/TransitionController.cs
@@ -11,27 +11,86 @@ public class TransitionController : MonoBehaviour {
     public Camera alienCam;
 
    public RawImage transImage;
-    public Material camMaterial;
+   public Material camMaterial;
+
+    public GameObject alien;
+    public GameObject human;
+    public bool isAlien;
+
+    private bool transitioning, grow;
+    private float shaderValue, newShaderValue;
+
+    private RenderTexture test;
 
     // Use this for initialization
     void Start () {
-        humanCam.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        test =  new RenderTexture(Screen.width, Screen.height, 24);
+        humanCam.targetTexture = test;
         transImage.material.mainTexture = humanCam.targetTexture;
-
-
         camMaterial = transImage.material;
+        isAlien = true;
+        transitioning = false;
+        newShaderValue = camMaterial.GetFloat("_Clipping");
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("e"))
+        shaderValue = camMaterial.GetFloat("_Clipping");
+        if (Input.GetKeyDown("e") && !transitioning)
         {
             Transition();
+            transitioning = true;
+        }
+
+        if (isAlien)
+        {
+            human.transform.localPosition = alien.transform.localPosition;
+        } else
+        {
+            alien.transform.localPosition = human.transform.localPosition;
+        }
+
+        if (transitioning)
+        {
+            if (grow && shaderValue < 1.35f)
+            {
+                newShaderValue += Time.deltaTime * 2;
+            }
         }
 	}
 
     public void Transition()
     {
-        camMaterial.SetFloat("_Clipping", 0.4f);
+        if (isAlien)
+        {
+
+            human.GetComponent<Rigidbody>().detectCollisions = true;
+            human.GetComponent<Rigidbody>().velocity = alien.GetComponent<Rigidbody>().velocity;
+            alien.GetComponent<Rigidbody>().detectCollisions = false;
+
+            humanCam.targetTexture = null;
+            alienCam.targetTexture = test;
+            transImage.material.mainTexture = alienCam.targetTexture;
+            camMaterial = transImage.material;
+
+            isAlien = !isAlien;
+        } else
+        {
+
+            alien.GetComponent<Rigidbody>().detectCollisions = true;
+            alien.GetComponent<Rigidbody>().velocity = human.GetComponent<Rigidbody>().velocity;
+            human.GetComponent<Rigidbody>().detectCollisions = false;
+
+            alienCam.targetTexture = null;
+            humanCam.targetTexture = test;
+            transImage.material.mainTexture = humanCam.targetTexture;
+            camMaterial = transImage.material;
+
+
+            //humanCam.depth = 0;
+            //alienCam.depth = 1;
+            isAlien = !isAlien;
+        }
+        
     }
 }
