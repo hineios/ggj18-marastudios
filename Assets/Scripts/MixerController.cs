582fc0394491ecdc4e1ae7e3e7d2d6b18d16d47d
Fábio Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2018-01-27 21:23:59 +0000

    Alienworld with muffled song and volume changes with distance

--- Assets/Scripts/MixerController.cs
+++ Assets/Scripts/MixerController.cs
@@ -5,19 +5,49 @@ using UnityEngine.Audio;
 
 public class MixerController : MonoBehaviour {
 
+    public const float LP_ACTIVE = 300f;
+    public const float LP_INACTIVE = 22000f;
     public GameObject Listener;
     public GameObject AudioSource;
     public AudioMixer Mixer;
+    public GameObject Transition;
+    private TransitionController t;
+    private bool HasLowPass = true;
 
-    void SetLowpassCutoff(float c)
+    private void Awake()
     {
-        Mixer.SetFloat("LowpassCutoff", c);
+        t = Transition.GetComponent<TransitionController>();
+    }
+
+    void SetLowpassCutoff(bool b)
+    {
+        if(HasLowPass != b)
+        {
+            HasLowPass = !HasLowPass;
+            if (HasLowPass)
+            {
+                Mixer.SetFloat("LowpassCutoff", LP_ACTIVE);
+            }
+            else
+            {
+                Mixer.SetFloat("LowpassCutoff", LP_INACTIVE);
+            }
+            
+        }
+            
+    }
+
+    void SetVolume(float c)
+    {
+        Mixer.SetFloat("Volume", c);
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        SetLowpassCutoff(t.isAlien);
         float d = Mathf.Abs(Listener.transform.position.x - AudioSource.transform.position.x);
 
-        SetLowpassCutoff(Mathf.Clamp(6000/d, 100, 3000));
+        SetVolume(Mathf.Clamp(-d , -40, 0));
 	}
 }
