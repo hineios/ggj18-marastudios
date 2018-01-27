a78b0a66e4a6d02b6ba8f164b9be8fd768409321
Baltazar95 <tiago_mpb95@hotmail.com>
2018-01-27 21:29:01 +0000

    changing level

---
+++ Assets/Scripts/MilkshakeSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkshakeSpawner : MonoBehaviour {
+
+    public GameObject prefab;
+
+    private GameObject player;
+
+    private int countdown = 0;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player_human");
+        prefab = GameObject.Find("Milkshake");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Mathf.Abs(player.transform.position.x - transform.position.x) <= 20 && Mathf.Abs(player.transform.position.x - transform.position.x) > 5)
+        {
+            if (countdown == 0)
+            {
+                Instantiate(prefab, transform.position, Quaternion.identity);
+                countdown = 150;
+            }
+            else
+            {
+                countdown--;
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/ShitshakeSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShitshakeSpawner : MonoBehaviour {
+
+    public GameObject prefab;
+
+    private GameObject player;
+
+    private int countdown = 0;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player_alien");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Mathf.Abs(player.transform.position.x - transform.position.x) <= 20 && Mathf.Abs(player.transform.position.x - transform.position.x) > 5)
+        {
+            if (countdown == 0)
+            {
+                GameObject t = Instantiate(prefab, transform.position, transform.rotation);
+                countdown = 150;
+            }
+            else
+            {
+                countdown--;
+            }
+        }
+
+    }
+}
