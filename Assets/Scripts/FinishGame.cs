eac39888a4129e02aed06e233fa305e7c1322714
Fábio Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2018-01-28 12:19:43 +0000

    Final room and disc collected

--- Assets/Scripts/FinishGame.cs
+++ Assets/Scripts/FinishGame.cs
@@ -7,13 +7,17 @@ public class FinishGame : MonoBehaviour
     public GameObject Canvas;
     public GameObject Music;
     public GameObject TeleportDestination;
+    public GameObject FinalRoom;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            
             Canvas.SetActive(false);
+            FinalRoom.SetActive(true);
             other.transform.position = TeleportDestination.transform.position;
+
         }
     }
 }
