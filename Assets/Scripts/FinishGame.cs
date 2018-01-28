103da9c08ac2ed1c59fc5b58585e0641b6bfb9be
Fábio Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2018-01-28 12:04:50 +0000

    added final room and teleport

---
+++ Assets/Scripts/FinishGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishGame : MonoBehaviour
+{
+    public GameObject Canvas;
+    public GameObject Music;
+    public GameObject TeleportDestination;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Canvas.SetActive(false);
+            other.transform.position = TeleportDestination.transform.position;
+        }
+    }
+}
