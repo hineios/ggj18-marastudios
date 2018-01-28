8d03201b282e813ef46fb772c6a890898202dc34
Baltazar95 <tiago_mpb95@hotmail.com>
2018-01-28 00:50:38 +0000

    level layout finished

---
+++ Assets/MilkshakeDestroyer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkshakeDestroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.transform.name.Contains("shake"))
+        {
+            Debug.Log(other.gameObject.transform.name);
+            Destroy(other.gameObject);
+        }
+    }
+}
--- Assets/Scripts/YMovingPlatform.cs
+++ Assets/Scripts/YMovingPlatform.cs
@@ -12,6 +12,6 @@ public class YMovingPlatform : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //transform.position = new Vector3(Mathf.PingPong(Time.time, 1), transform.position.y, transform.position.z);
-        transform.Translate(transform.up * Mathf.Cos(Time.time) * Time.deltaTime * 2);
+        transform.Translate(transform.up * Mathf.Cos(Time.time) * Time.deltaTime);
     }
 }
